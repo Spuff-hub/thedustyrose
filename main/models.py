ab8ae71b5a3e76f727b731a1b920b5643af452e6
Emir Los <emir.lotfullin@yandex.ru>
2026-03-29 17:53:58 +0300

    executed views/urls/admin

--- main/models.py
+++ main/models.py
@@ -17,7 +17,7 @@ class Size(models.Model):
     name = models.CharField(max_length=20)
     
     def __str__(self):
-        return self.name  # Было safe.name, исправлено на self.name
+        return self.name
 
 class ProductSize(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE,
